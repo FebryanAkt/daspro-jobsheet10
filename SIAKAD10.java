483432b3c4f8372c8288297fc543e0b4f89694b7
FebryanAkt <febryan08022006@gmail.com>
2024-11-08 10:22:50 +0700

    Pertanyaan percobaan 4

--- SIAKAD10.java
+++ SIAKAD10.java
@@ -3,8 +3,15 @@ public class SIAKAD10 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int jmlSiswa, jmlMatkul;
 
-        int [][] nilai = new int [4][3];
+        System.out.print("Masukkan jumlah siswa: ");
+        jmlSiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        jmlMatkul = sc.nextInt();
+
+        
+        int [][] nilai = new int [jmlSiswa][jmlMatkul];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i+1));
@@ -15,18 +22,18 @@ public class SIAKAD10 {
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " +totalPerSiswa/3);
+            System.out.println("Nilai rata-rata: " +totalPerSiswa/ jmlSiswa); 
         }
         System.out.println("\n=====================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jmlSiswa; j++) { 
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jmlMatkul; i++) { 
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jmlMatkul); 
         }
     }
 }
